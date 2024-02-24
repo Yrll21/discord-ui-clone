323701fbab08e0f0796d347a12dcf3c4a523b265
Yrll21 <ivanyrlllabadia@gmail.com>
2024-02-24 20:15:45 +0800

    Fine tuning animation and tooltip

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -6,19 +6,21 @@ import { FaFire, FaPoo } from "react-icons/fa";
 const SideBar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-primary text-secondary shadow-lg z-10">
-      <SidebarIcon icon={<FaFire size="28" />} />
-      <SidebarIcon icon={<BsPlus size="32" />} />
-      <SidebarIcon icon={<BsFillLightningFill size="20" />} />
-      <SidebarIcon icon={<FaPoo size="20" />} />
+      <SidebarIcon icon={<FaFire size="28" />}>Fire Icon!</SidebarIcon>
+      <SidebarIcon icon={<BsPlus size="32" />}>Plus Icon!</SidebarIcon>
+      <SidebarIcon icon={<BsFillLightningFill size="20" />}>
+        Lightning Icon!
+      </SidebarIcon>
+      <SidebarIcon icon={<FaPoo size="20" />}>Poop Icon!</SidebarIcon>
     </div>
   );
 };
 
-const SidebarIcon = ({ icon }: any, text = "tooltip <3") => (
+const SidebarIcon = ({ icon, children }: any) => (
   <div className="sidebar-icon group">
     {icon}
 
-    <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+    <span className="sidebar-tooltip group-hover:scale-100">{children}</span>
   </div>
 );
 
